81d1e6882d013efe38fec9b5eb287b29f5be37da
Keng3021 <keng.saow60@sskru.ac.th>
2020-07-29 18:59:29 +0700

    make it better

--- Enroll/urls.py
+++ Enroll/urls.py
@@ -3,10 +3,10 @@ from Enroll import views
 
 
 urlpatterns = [
-    path('wapviwe', views.wapview, name='wap'),
+    path('wapviwe',views.Home,name='home'),
     path('temple_List', views.templeList, name='temple'),
     path('Category_List',views.CategoryList, name='Category'),
     path('tem_List', views.temList, name='tem'),
-    path('',views.Home,name='home'),
+    path('', views.wapview, name='wap'),
  
 ]
